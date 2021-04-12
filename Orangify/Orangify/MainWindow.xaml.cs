db9edb0a671e36bda06ba482628da9d39a364a5d
Tee <51485909+classactribute@users.noreply.github.com>
2021-04-11 20:44:22 -0400

    Installed Material Design extension from NuGet

--- Orangify/Orangify/MainWindow.xaml.cs
+++ Orangify/Orangify/MainWindow.xaml.cs
@@ -10,17 +10,16 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace Orangify
 {
     /// <summary>
-    /// Interaction logic for UserControl1.xaml
+    /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class UserControl1 : UserControl
+    public partial class MainWindow : Window
     {
-        public UserControl1()
+        public MainWindow()
         {
             InitializeComponent();
         }
