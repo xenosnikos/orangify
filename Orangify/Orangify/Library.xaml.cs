4a736ef0ff69894c6c04b6aac237b2e13070c618
Tee <51485909+classactribute@users.noreply.github.com>
2021-04-13 09:47:13 -0400

    Library
    Deleted Playlist, made Library

--- Orangify/Orangify/Library.xaml.cs
+++ Orangify/Orangify/Library.xaml.cs
@@ -10,17 +10,16 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace Orangify
 {
     /// <summary>
-    /// Interaction logic for RotaryDial.xaml
+    /// Interaction logic for Library.xaml
     /// </summary>
-    public partial class RotaryDial : UserControl
+    public partial class Library : Window
     {
-        public RotaryDial()
+        public Library()
         {
             InitializeComponent();
         }
