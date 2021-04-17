30f83c5887aff78ab9e04206a399f0472bc64246
gataki <nxenos@me.com>
2021-04-17 19:16:16 -0400

    Volume

--- Orangify/Orangify/Equalizer.xaml.cs
+++ Orangify/Orangify/Equalizer.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -30,6 +32,27 @@ namespace Orangify
             {
                 this.DragMove();
             }
+        }
+
+        private void Slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            // Calculate the volume that's being set
+            double newVolume = ushort.MaxValue * e.NewValue / 10.0;
+
+            uint v = ((uint)newVolume) & 0xffff;
+            uint vAll = v | (v << 16);
+
+            // Set the volume
+            int retVal = NativeMethods.WaveOutSetVolume(IntPtr.Zero, vAll);
+
+            Debug.WriteLine(retVal);
+        }
+
+        static class NativeMethods
+        {
+            [DllImport("winmm.dll", EntryPoint = "waveOutSetVolume")]
+            public static extern int WaveOutSetVolume(IntPtr hwo, uint dwVolume);
+
         }
     }
 }
